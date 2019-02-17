2681874a7d47a6584e80c469f5d19bfb43d8f3c9
klkirstyleeskl <k.lees@btinternet.com>
2019-02-17 16:15:07 +0000

    pases multiple changes test

--- src/test/java/HammingTest.java
+++ src/test/java/HammingTest.java
@@ -15,8 +15,22 @@ public class HammingTest {
     @Test
     public void noChangesInString() {
         String str1 = "AGU";
-        String str2 = "AGG";
+        String str2 = "AGU";
+        assertEquals(0, hamming.hammingDist(str1, str2));
+    }
+
+    @Test
+    public void oneChangeInString(){
+        String str1 = "AGG";
+        String str2 = "AGU";
         assertEquals(1, hamming.hammingDist(str1, str2));
+    }
+
+    @Test
+    public void moreThanOneChangeInString(){
+        String str1 = "ACA";
+        String str2 = "AGU";
+        assertEquals(2, hamming.hammingDist(str1, str2));
     }
 
 
